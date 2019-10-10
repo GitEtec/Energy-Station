62537a027e6bfa066d05a46f60bbe0c524d6effc
guto-alves <gustavo.almeida13@fatec.sp.gov.br>
2019-10-10 02:40:38 -0300

    Criar tela inicial

---
+++ EnergyStation/app/src/main/java/deb/px/com/example/energystation/MainActivity.java
@@ -0,0 +1,20 @@
+package deb.px.com.example.energystation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openMap(View view) {
+        startActivity(new Intent(MainActivity.this, MapsActivity.class));
+    }
+}
