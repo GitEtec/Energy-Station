15d10a39974176ebf6eb715bd7bf405512df46df
guto-alves <gustavo.almeida13@fatec.sp.gov.br>
2019-11-05 02:04:55 -0300

    Implementar uma Splash Screen

---
+++ app/src/main/java/deb/px/com/example/energystation/SplashActivity.java
@@ -0,0 +1,15 @@
+package deb.px.com.example.energystation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+    }
+}
